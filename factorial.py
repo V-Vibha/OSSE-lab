20e9d236fe79d824c7f15b9c057dca4efd60e20f
Jeffrey Rujen R <143261841+93085-JeffreyRujen@users.noreply.github.com>
2023-09-16 16:41:08 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,2 +1,13 @@
+def factorial(n):
+    if n < 0:
+        print("Sorry, factorial does not exist for negative numbers")
+    elif n == 0:
+        print("The factorial of 0 is 1")
+    else:
+        if n == 1:
+            return n
+        else:
+            return n*factorial(n-1)
+
 n = 5
-print(5*4*3*2*1)
+print("The factorial of", n, "is", factorial(n))
